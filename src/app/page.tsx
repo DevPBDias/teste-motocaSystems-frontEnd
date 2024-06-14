c35abf72eb6ae3daa1e6d1b24e6c5015d1f6115b
devpbdias <devpbdias@gmail.com>
2024-06-14 00:14:26 -0300

    feat: created card for motorcycles and fetch data to list motos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
 import Btn from "@/components/Btn";
+import MotoCard from "@/components/MotoCard";
 import PageTitle from "@/components/PageTitle";
 import SearchInput from "@/components/SearchInput";
 
-export default function Home() {
+interface MotoProps {
+  id: string;
+  name: string;
+  color: string;
+  value: string;
+  status: string;
+}
+
+async function getData() {
+  const res = await fetch("http://localhost:8000/motorcycles");
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
-    <section>
+    <main>
       <PageTitle title="Tabela de Motos" />
       <div>
         <SearchInput />
         <Btn text="novo registro" />
       </div>
-    </section>
+      <section>
+        {data.map((moto: MotoProps) => (
+          <MotoCard key={moto.id} moto={moto} />
+        ))}
+      </section>
+    </main>
   );
 }
---
+++ src/components/MotoCard.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import trashIcon from "../../public/icons/trash.svg";
+import eyeIcon from "../../public/icons/eye.svg";
+
+interface DataProps {
+  id: string;
+  name: string;
+  color: string;
+  value: string;
+  status: string;
+}
+
+const MotoCard = ({ moto }: any | DataProps) => {
+  return (
+    <div>
+      <div>
+        <p>#{moto?.id}</p>
+        <div>
+          <div>
+            <p>{moto?.name}</p>
+            <p>{moto?.status}</p>
+          </div>
+          <p>Valor: R$ {moto?.value}</p>
+          <p>Cor: {moto?.color}</p>
+        </div>
+      </div>
+      <div>
+        <button type="button">
+          <Image src={trashIcon} alt="trash Icon" />
+        </button>
+        <button type="button">
+          <Image src={eyeIcon} alt=" eyeIcon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MotoCard;
